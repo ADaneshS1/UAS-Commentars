e7d28e19c8c87b8368aadd77c1c2e073dcab1d05
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2024-06-07 10:57:07 +0700

    tampilan UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,124 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [allData,setAllData] = useState()
+  const router = useRouter()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const nama = event.target.nama.value
+    const komentar = event.target.komentar.value
+    const website = event.target.website.value
+
+    fetch(`/api/insertData`, {
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body: JSON.stringify({
+        nama:nama,
+        komentar:komentar,
+        website:website
+      }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.message)
+      router.reload()
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  }
+
+  useEffect(() => {
+    fetch(`/api/selectData`)
+    .then((res) => res.json())
+    .then((data) => {
+      setAllData(data.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div style={{ marginTop:"50px"}} className="flex flex-col">
+
+      <div className="">
+        <h1 style={{ fontSize:"28px", fontFamily:"cursive", textAlign:"center" }}>Profile</h1>
+        <p style={{ fontSize:"16px", textAlign:"center" }}>Halo, nama saya Ahmad Danesh Sudiono. Saya seorang programmer pemula yang masih belajar tentang frontend dan backend. Tolong tanggapi karya-karya saya disini. Terima kasih</p>
+
+        <div className="flex flex-row justify-center gap-6 mt-5">
+          <div className="flex flex-col">
+            <p style={{ fontSize:"23px", fontFamily:"monospace" }}>Projek</p>
+            <a href="https://pencatat-uang.vercel.app/">Pencatatan Uang</a>
+            <a href="https://daynote-beta.vercel.app/">Catatan Harian</a>
+            <a href="https://absensi-karyawan-beryl.vercel.app/">Absensi Karywan</a>
+          </div>
+          
+          <div>
+            <p style={{ fontSize:"23px", fontFamily:"monospace" }}>Artikel</p>
+            <a href="https://medium.com/@ahmaddaneshsudiono1.ppqita/testing-api-dengan-thunder-client-4038ac04b8d5">Testing API Dengan Thunder Client di NextJs</a>
+            <a href="https://medium.com/@ahmaddaneshsudiono1.ppqita/testing-api-dengan-thunder-client-4038ac04b8d5">Testing API Dengan Thunder Client di NextJs</a>
+          </div>
+
+          <div>
+            <p style={{ fontSize:"23px", fontFamily:"monospace" }}>Linkedin</p>
+            <a href="https://www.linkedin.com/in/danesssh">Linkedin</a>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-10 border-solid border-2 border-black p-10 mx-3">
+        <div style={{ fontFamily:"cursive", textAlign:"center", fontSize:"28px" }}>
+          <h1>Buat Komentar</h1>
+        </div>
+
+        <form onSubmit={handleSubmit} className="flex flex-row justify-center gap-4 mt-10">
+
+          <div className="border-solid border-2 border-black px-2 py-1 rounded-md">
+            <input placeholder="Nama" name="nama" required></input>
+          </div>
+
+          <div className="border-solid border-2 border-black px-2 py-1 rounded-md">
+            <input placeholder="Komentar" name="komentar" required></input>
+          </div>
+
+          <div className="border-solid border-2 border-black px-2 py-1 rounded-md">
+            <input placeholder="Website" name="website"></input>
+          </div>
+
+          <button type="submit" className="border-solid border-2 border-black px-2 py-1 rounded-md">Buat</button>
+
+        </form>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="mt-20">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div style={{ fontFamily:"cursive", textAlign:"center", fontSize:"28px" }}>
+          <h1>Daftar Komentar</h1>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        {allData === null && <h3>Data Kosong</h3> }
+        {allData === undefined && <h1 style={{ fontSize:"30px" }}>Loading...</h1> }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {allData && allData.map((data,index) => {
+          return (
+            <div key={index} className="font-mono my-5 mx-3 p-3 border-solid border-2 border-black">
+              <p>ID: {data.id}</p>
+              <p>Nama: {data.nama}</p>
+              <p>Komentar: {data.komentar}</p>
+              <p>Website: <a href={data.website}>{data.website} </a> </p>
+            </div>
+          )
+        })}
+
       </div>
-    </main>
-  );
+     
+    </div>
+
+
+  )
 }
